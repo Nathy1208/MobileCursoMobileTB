b4ac8e8519d5696eea3e049309d8d120e81d43ca
Nathy1208 <smaniotonathaly0@gmail.com>
2025-10-09 11:27:24 -0300

    09/10/2025

--- Hardware/sa_localizacao_mapa/lib/controllers/map_controller.dart
+++ Hardware/sa_localizacao_mapa/lib/controllers/map_controller.dart
@@ -4,29 +4,34 @@ import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
 import 'package:sa_localizacao_mapa/models/location_point.dart';
 
-class MapController {
+class PointController {
 
   final DateFormat _formatar = DateFormat("dd/MM/yyyy - HH:mm:ss");
   
 
-  void _getCurrentLocation() async{
+  Future<LocationPoint> pegarPonto() async{
     // solictar a localização atual do sipositivo 
     // liberar permissão
     bool serviceEnabled;
     LocationPermission permission;
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if(!serviceEnabled){
-      permission = await Geolocator.checkPermission();
+      throw Exception("Serviço de GPS não Habilitado");
+    }
+    permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied){
+      permission == await Geolocator.requestPermission();
       if(permission == LocationPermission.denied){
-        permission == await Geolocator.requestPermission();
+        throw Exception("Permissão ao GPS Negada");
       }
     }
     Position position = await Geolocator.getCurrentPosition();
 
     String dataHora = _formatar.format(DateTime.now());
     LocationPoint ponto = LocationPoint(
-      latitude: position.latitude,
+      latitude: position.latitude, 
       longitude: position.longitude, 
-      timestamp: dataHora);   
+      timestamp: dataHora);
+    return ponto;
   }
 }
--- Hardware/sa_localizacao_mapa/lib/main.dart
+++ Hardware/sa_localizacao_mapa/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:sa_localizacao_mapa/views/map_view.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: LocationScreen(),
+    home: MapView(),
     debugShowCheckedModeBanner: false,
   ));
 }
